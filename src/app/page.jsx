1a8d749ebd636a001aba39ac3ac7a26a2edbb336
hirajya <rodneyestrada2425@gmail.com>
2024-12-14 00:33:17 +0800

    added lines to assurance

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -75,29 +75,39 @@ const Page = () => {
         }
       `}</style>
 
-      {/* Guarantee Assurance Part */}
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-12 px-3">
-          <AssuranceElement
-            src="/homepage/AssuranceLogo1.png"
-            title="Your Most Trusted Store"
-            description="100% All Brand New & Original Shop with Confidence."
-          />
-          <AssuranceElement
-            src="/homepage/AssuranceLogo2.png"
-            title="Fast Shipping Nationwide"
-            description="Ships in 24 hours! Express and Same-Day Delivery within Metro Manila!"
-          />
-          <AssuranceElement
-            src="/homepage/AssuranceLogo3.png"
-            title="Save on Loyalty Rewards"
-            description="Sign in to start earning Loyalty Rewards Points!"
-          />
-          <AssuranceElement
-            src="/homepage/AssuranceLogo4.png"
-            title="100% Safe and Secure"
-            description="All Transactions are Fully Encrypted with State of the Art Technology!"
-          />
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 px-3">
+          <div className="relative pr-6 md:pr-8">
+            <AssuranceElement
+              src="/homepage/AssuranceLogo1.png"
+              title="Your Most Trusted Store"
+              description="100% All Brand New & Original Shop with Confidence."
+            />
+            <div className="absolute inset-y-0 right-0 w-px bg-gray-400 hidden md:block"></div>
+          </div>
+          <div className="relative pr-6 md:pr-8">
+            <AssuranceElement
+              src="/homepage/AssuranceLogo2.png"
+              title="Fast Shipping Nationwide"
+              description="Ships in 24 hours! Express and Same-Day Delivery within Metro Manila!"
+            />
+            <div className="absolute inset-y-0 right-0 w-px bg-gray-400 hidden md:block"></div>
+          </div>
+          <div className="relative pr-6 md:pr-8">
+            <AssuranceElement
+              src="/homepage/AssuranceLogo3.png"
+              title="Save on Loyalty Rewards"
+              description="Sign in to start earning Loyalty Rewards Points!"
+            />
+            <div className="absolute inset-y-0 right-0 w-px bg-gray-400 hidden md:block"></div>
+          </div>
+          <div>
+            <AssuranceElement
+              src="/homepage/AssuranceLogo4.png"
+              title="100% Safe and Secure"
+              description="All Transactions are Fully Encrypted with State of the Art Technology!"
+            />
+          </div>
         </div>
       </div>
     </div>

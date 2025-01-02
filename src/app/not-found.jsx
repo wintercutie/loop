fdf50d2d2e1147623f676926390b93a0db227bda
rajkyutiebaby <rodneyestrada2425@gmail.com>
2025-01-02 16:29:07 +0800

    not-found.jsx and error.jsx done

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function ErrorPage() {
+export default function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-100 via-gray-200 to-gray-300">
       <div className="text-center text-gray-800 max-w-lg mx-auto">
         <h1 className="text-6xl font-serif font-semibold mb-6">404</h1>
         <h2 className="text-2xl font-medium mb-6">Sorry, this page isn’t available</h2>
         <p className="text-lg mb-8">
-          ERROR!
+          The page you are looking for might have been moved or doesn’t exist anymore. 
           Let’s get you back to where you belong.
         </p>
 
